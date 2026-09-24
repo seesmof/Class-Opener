b18eb179ecf26398a670d0a12ea9de6c4c54cbcb
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-24 14:24:31 +0300

    refactor: make the code functional

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# My thoughts are not really anything so im thinking of Jesus Christ my Lord Amen! So okay so we need a some kind of a text file where we will store the classes data. Which file format sohuld we use? json? maybe csv though? csv is like a table. okay so i created a text file that will show me this thing. oaky now i wanna read the file for this i will need a current folder. okay yeah i need to read the file. okay so when trying to access the file i have an issue where i not always have the url or the code but i cant account for that, i need to decouple the 3 arguments in case they are there. one option would be to use a pandas dataframe because it allows for missing values. althogh this might be an overkill for the schedule that will need to access. created a csv file but there's still missing values so i should maybe just fill them in with the hoax values or create a json file where i would be able to just assign an empty string to the thing. okay it turns out the .split python method has a max split param. although i have no idea how to use it. lets try the json option.
+# My thoughts are not really anything so im thinking of Jesus Christ my Lord Amen! So okay so we need a some kind of a text file where we will store the classes data. Which file format sohuld we use? json? maybe csv though? csv is like a table. okay so i created a text file that will show me this thing. oaky now i wanna read the file for this i will need a current folder. okay yeah i need to read the file. okay so when trying to access the file i have an issue where i not always have the url or the code but i cant account for that, i need to decouple the 3 arguments in case they are there. one option would be to use a pandas dataframe because it allows for missing values. althogh this might be an overkill for the schedule that will need to access. created a csv file but there's still missing values so i should maybe just fill them in with the hoax values or create a json file where i would be able to just assign an empty string to the thing. okay it turns out the .split python method has a max split param. although i have no idea how to use it. lets try the json option. okay the class opening works
 
 from dataclasses import dataclass
 import json
@@ -6,7 +6,11 @@ import os
 import time
 import schedule
 
+# Global Variables
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
+
+# Helper Functions
 def open_in_browser(url: str):
     os.system(f'start "" {url}')
 
@@ -15,6 +19,7 @@ def copy_to_clipboard(text: str):
     os.system(f"echo {text.strip()}| clip")
 
 
+# Data Models
 @dataclass
 class Entry:
     title_and_type: str
@@ -22,29 +27,39 @@ class Entry:
     code: str
 
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-file_name = "data.json"
-file_path = os.path.join(current_dir, file_name)
-classes_data: list[Entry] = list()
-with open(file_path, encoding="utf-8", mode="r") as f:
-    lines = json.load(f)
-    for line in lines:
-        entry = Entry(
-            title_and_type=line["title_and_type"], url=line["url"], code=line["code"]
-        )
-        classes_data.append(entry)
-classes_data = sorted(classes_data, key=lambda entry: entry.title_and_type)
+def populate_classes_data(data_file_name: str = "data.json") -> list[Entry]:
+    classes_data: list[Entry] = list()
+    file_path = os.path.join(current_dir, data_file_name)
+
+    with open(file_path, encoding="utf-8", mode="r") as f:
+        lines = json.load(f)
+        print(lines[0])
+        for line in lines:
+            entry = Entry(
+                title_and_type=line["title_and_type"],
+                url=line["url"],
+                code=line["code"],
+            )
+            classes_data.append(entry)
+    classes_data = sorted(classes_data, key=lambda entry: entry.title_and_type)
+
+    return classes_data
 
 
 def open_class(class_data: Entry):
     open_in_browser(class_data.url)
-    copy_to_clipboard(class_data.code)
+    copy_to_clipboard(class_data.code) if class_data.code else ""
 
     class_name, type = class_data.title_and_type.split(" ")
     print(f'Opening {class_name}, it is {"Практика" if type=="P" else "Лекція"}')
 
 
-open_class(classes_data[0])
+def main():
+    classes_data = populate_classes_data()
+
+
+if __name__ == "__main__":
+    main()
 
 """
 schedule.every().wednesday.at("16:25").do(open_tznk)
